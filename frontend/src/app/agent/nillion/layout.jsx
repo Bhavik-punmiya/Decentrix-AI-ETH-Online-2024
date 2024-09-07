0f4cbc2de52f6b93989658b39755c0c0c982e527
shantanuSakpal <shantanuesakpal1420@gmail.com>
2024-09-07 11:22:25 +0530

    morph agent added

---
+++ frontend/src/app/agent/nillion/layout.jsx
@@ -0,0 +1,17 @@
+import Sidebar from '@/components/Sidebar';
+import { ContractProvider } from '@/contexts/ContractContext';
+
+const Layout = ({ children }) => {
+  return (
+    <ContractProvider>
+        <div className="flex">
+            <Sidebar/>
+            <main className="flex-1 ml-16 p-4">
+                {children}
+            </main>
+        </div>
+    </ContractProvider>
+  );
+};
+
+export default Layout;
