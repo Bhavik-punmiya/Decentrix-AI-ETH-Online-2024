a2fe75275a7d43c90b48dff462b5654bc30aaa38
Bhavik-punmiya <BhavikPunmiya@gmail.com>
2024-09-07 11:47:02 +0530

    updated landing page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -29,7 +29,9 @@ function App() {
             <div className="bg-theme-off-white-light rounded-xl p-10 w-full mt-24">
                 <div className="w-full text-center text-4xl mb-10" id="agents"><span>Choose an agent</span></div>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-7">
+                {/* Grid for initial 6 agents */}
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
+                    {/* Rootstock Agent */}
                     <div className="bg-theme-green-light rounded-xl shadow-lg p-5">
                         <div className="flex w-full justify-between items-center">
                             <div className="flex items-center gap-1">
@@ -45,6 +47,8 @@ function App() {
                             Rootstock is the top and oldest running Bitcoin Layer 2 Blockchain, fully EVM compatible.
                         </div>
                     </div>
+
+                    {/* Hedera Agent */}
                     <div className="bg-theme-purple-light rounded-xl shadow-lg p-5">
                         <div className="flex w-full justify-between items-center">
                             <div className="flex items-center gap-3">
@@ -60,6 +64,8 @@ function App() {
                             An open-source, public network governed by leading organizations around the world.
                         </div>
                     </div>
+
+                    {/* Blockless Agent */}
                     <div className="bg-theme-gray-light rounded-xl shadow-lg p-5">
                         <div className="flex w-full justify-between items-center">
                             <div className="flex items-center gap-1">
@@ -73,11 +79,11 @@ function App() {
                             </Link>
                         </div>
                         <div className="font-light text-lg mt-3">
-                            Blockless is the infrastructure platform to launch, integrate, and secure Network Neutral
-                            Applications (nnApps).
+                            Blockless is the infrastructure platform to launch, integrate, and secure Network Neutral Applications (nnApps).
                         </div>
                     </div>
 
+                    {/* Morph Agent */}
                     <div className="bg-theme-gray-light rounded-xl shadow-lg p-5">
                         <div className="flex w-full justify-between items-center">
                             <div className="flex items-center gap-3">
@@ -91,11 +97,11 @@ function App() {
                             </Link>
                         </div>
                         <div className="font-light text-lg mt-3">
-                            Morph is a blockchain platform focused on seamless asset exchanges and decentralized finance
-                            solutions.
+                            Morph is a blockchain platform focused on seamless asset exchanges and decentralized finance solutions.
                         </div>
                     </div>
 
+                    {/* Nilion Agent */}
                     <div className="bg-theme-green-light rounded-xl shadow-lg p-5">
                         <div className="flex w-full justify-between items-center">
                             <div className="flex items-center gap-4">
@@ -113,6 +119,7 @@ function App() {
                         </div>
                     </div>
 
+                    {/* Nethermind Agent */}
                     <div className="bg-theme-purple-light rounded-xl shadow-lg p-5">
                         <div className="flex w-full justify-between items-center">
                             <div className="flex items-center gap-4">
@@ -126,16 +133,46 @@ function App() {
                             </Link>
                         </div>
                         <div className="font-light text-lg mt-3">
-                            Nethermind is a blockchain development company that provides Ethereum infrastructure and
-                            tools.
+                            Nethermind is a blockchain development company that provides Ethereum infrastructure and tools.
+                        </div>
+                    </div>
+                </div>
+
+                {/* Flex container for the last 2 agents */}
+                <div className="flex justify-center items-center mt-10 gap-7">
+                    {/* Chainlink Agent */}
+                    <div className="bg-theme-blue-light rounded-xl shadow-lg p-5 w-80">
+                        <div className="flex w-full justify-between items-center">
+                            <div className="flex items-center gap-2">
+                                <Image src="/chain/chainlink-logo.png" alt="chainlink" width={40} height={40} />
+                                <div className="text-xl text-center font-bold">Chainlink</div>
+                            </div>
+                            <Link href={"/agent/chainlink/chat"}
+                                className="text-2xl p-2 rounded-xl bg-theme-blue-dark text-white hover:scale-110 ease-out transition-all">
+                                <FaTelegramPlane />
+                            </Link>
+                        </div>
+                        <div className="font-light text-lg mt-3">
+                            Chainlink is the most widely used oracle network for powering hybrid smart contracts.
                         </div>
                     </div>
 
-                </div>
-
-                {/* New Divs for Morph, Nethermind, and Nilion */}
-                <div className="grid grid-cols-3 space-x-10 mt-10">
-
+                    {/* Fhenix Agent */}
+                    <div className="bg-theme-orange-light rounded-xl shadow-lg p-5 w-80">
+                        <div className="flex w-full justify-between items-center">
+                            <div className="flex items-center gap-2">
+                                <Image src="/chain/fhenix-logo.png" alt="fhenix" width={40} height={40} />
+                                <div className="text-xl text-center font-bold">Fhenix</div>
+                            </div>
+                            <Link href={"/agent/fhenix/chat"}
+                                className="text-2xl p-2 rounded-xl bg-theme-orange-dark text-white hover:scale-110 ease-out transition-all">
+                                <FaTelegramPlane />
+                            </Link>
+                        </div>
+                        <div className="font-light text-lg mt-3">
+                            Fhenix is a scalable blockchain platform designed for Web3 innovation and development.
+                        </div>
+                    </div>
                 </div>
             </div>
         </main>
