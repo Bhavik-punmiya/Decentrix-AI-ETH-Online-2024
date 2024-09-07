21016c758731c00c5ccbabfd310159c3d3953a6c
Bhavik-punmiya <BhavikPunmiya@gmail.com>
2024-09-07 13:23:06 +0530

    coming soon ! Stay Tuned

--- frontend/src/app/agent/fhenix/layout.jsx
+++ frontend/src/app/agent/fhenix/layout.jsx
@@ -5,10 +5,10 @@ const Layout = ({ children }) => {
   return (
     <ContractProvider>
       <div className="flex">
-        <Sidebar />
-        <main className="flex-1 ml-16 p-4">
-          {children}
-        </main>
+        {/* <Sidebar /> */}
+        <div className="h-full flex justify-center items-center text-5xl font-bold py-24">
+                Coming soon !
+            </div>
       </div>
     </ContractProvider>
   );
